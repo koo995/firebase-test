5439205ee7a6301cc08e61db5b50f1c147827aeb
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-01 19:02:59 +0900

    firebase 스프링빈으로 등록

--- src/main/java/test/firebase/auth/FirebaseInitializer.java
+++ src/main/java/test/firebase/auth/FirebaseInitializer.java
@@ -3,29 +3,29 @@ package test.firebase.auth;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 @Slf4j
-@Component
+@Configuration
 public class FirebaseInitializer {
 
     @Value("${firebase.credential.resource-path}")
     private String keyPath;
 
-    @PostConstruct
-    public void firebaseApp() throws IOException {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
         log.info("Initializing Firebase.");
         InputStream inputStream = new ClassPathResource(keyPath).getInputStream();
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(inputStream))
                 .build();
-        FirebaseApp.initializeApp(options);
+        return FirebaseApp.initializeApp(options);
     }
 }
