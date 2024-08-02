54511fa396beccddcc33779e062be7f2d1dcfe2f
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-02 13:24:52 +0900

    완전히 성공

--- src/main/java/test/firebase/auth/AuthService.java
+++ src/main/java/test/firebase/auth/AuthService.java
@@ -2,19 +2,17 @@ package test.firebase.auth;
 
 
 import com.google.firebase.auth.FirebaseToken;
-import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Lock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import test.firebase.member.Member;
 import test.firebase.member.MemberRepository;
 
 import java.util.Optional;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,42 +21,25 @@ public class AuthService {
 
     private final MemberRepository memberRepository;
 
-    private final ReentrantLock lock = new ReentrantLock();
-
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    public Member joinAndLogin(FirebaseToken decodedToken) throws InterruptedException {
-        lock.lock();
+    @Retryable(
+            retryFor = {DataIntegrityViolationException.class},
+            backoff = @Backoff(delay = 1000)
+    )
+    @Transactional
+    public Member joinAndLogin(FirebaseToken decodedToken) {
         String uid = decodedToken.getUid();
-        Thread.sleep(1000);
         Optional<Member> memberOptional = memberRepository.findByUid(uid);
-
         if (memberOptional.isPresent()) {
-            log.info("이미 가입된 회원입니다.");
-            Member member = memberOptional.get();
-            lock.unlock();
-            return member;
-        } else {
-            log.info("새로운 회원입니다.");
-            Member newMember = Member.builder()
-                    .username(decodedToken.getName())
-                    .email(decodedToken.getEmail())
-                    .uid(uid)
-                    .build();
-            Member member = memberRepository.save(newMember);
-            lock.unlock();
-            return member;
+            log.info("기존 회원입니다.");
+            return memberOptional.get();
         }
+        log.info("새로운 회원입니다.");
+        Member member = Member.builder()
+                .username(decodedToken.getName())
+                .email(decodedToken.getEmail())
+                .uid(uid).build();
+        memberRepository.save(member);
+        return member;
     }
-
-
-//        Member member = memberRepository.findByUid(uid)
-//                .orElseGet(Member.builder()
-//                        .username(decodedToken.getName())
-//                        .email(decodedToken.getEmail())
-//                        .uid(uid)::build);
-//        memberRepository.save(member);
-//        lock.unlock();
-//        return member;
-
 }
+
